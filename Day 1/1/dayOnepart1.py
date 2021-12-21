0a1e7a72e4cd1e16c1acbb84d6e4fb2e345881a6
HuckRob <RobertBuentgen@u.boisestate.edu>
2021-12-20 18:00:39 -0700

    day 1 part 1 complete

--- Day 1/1/dayOnepart1.py
+++ Day 1/1/dayOnepart1.py
@@ -1,2 +1,19 @@
 import os
 
+#Makes a variable called location to store the location of the py file
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+file = open(os.path.join(__location__, 'data.txt')) 
+
+prevNumb = 0
+tally = 0
+
+
+for line in file.readlines():
+    if(int(line) > prevNumb):
+        prevNumb = int(line)
+        tally = tally + 1
+    else:
+        prevNumb = int(line)
+
+print(tally)
